6a2f03e3207ffbf3f628fbe710a9fc16f691e767
Aleksi <aleksi.suoranta@gmail.com>
2025-08-09 13:10:32 +0300

    Setup a plugin for the game state

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,16 @@ use bevy::prelude::*;
 pub struct AppPlugin;
 
 mod camera;
+mod game;
 mod game_state;
 
 impl Plugin for AppPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((DefaultPlugins, game_state::plugin, camera::plugin));
+        app.add_plugins((
+            DefaultPlugins,
+            game_state::plugin,
+            camera::plugin,
+            game::plugin,
+        ));
     }
 }
