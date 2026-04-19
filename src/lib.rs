ff1e1200898b4522ba05963dbac5b8ff5941572a
Aleksuo <aleksi.suoranta@gmail.com>
2026-04-19 09:26:12 +0000

    Use plugin, add some wasm specific code for handling assets

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,35 @@
-use bevy::prelude::*;
+use bevy::{asset::AssetMetaCheck, prelude::*};
 pub struct AppPlugin;
 
 mod camera;
 mod game;
 mod game_state;
+mod texture_atlas;
 
 impl Plugin for AppPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins((
-            DefaultPlugins.set(WindowPlugin {
-                primary_window: Some(Window {
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        #[cfg(target_arch = "wasm32")]
+                        canvas: Some("#bevy-canvas".to_string()),
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest())
+                .set(AssetPlugin {
                     #[cfg(target_arch = "wasm32")]
-                    canvas: Some("#bevy-canvas".to_string()),
+                    file_path: "/bMinesweeper/assets".into(),
+                    #[cfg(target_arch = "wasm32")]
+                    meta_check: AssetMetaCheck::Never,
                     ..default()
                 }),
-                ..default()
-            }),
             game_state::plugin,
             camera::plugin,
             game::plugin,
+            texture_atlas::plugin,
         ));
     }
 }
